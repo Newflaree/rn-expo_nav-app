866b440d3d5d45b92c5ca855af2e906840302058
Camilo López <camiloj.lopez79@gmail.com>
2025-01-05 10:54:40 -0300

    [INIT]: Init commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import {
+  View,
+  Text
+} from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text style={{ color: 'white' }}>Hola</Text>
+    </View>
+  );
+}
