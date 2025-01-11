2a816bae2b5629345f9bb7e31ee933d5cbc3138b
Camilo López <camiloj.lopez79@gmail.com>
2025-01-11 12:12:06 -0300

    Stack - PopToTop

--- app/src/presentation/screens/settings/SettingsScreen.tsx
+++ app/src/presentation/screens/settings/SettingsScreen.tsx
@@ -3,12 +3,33 @@ import {
   Text,
   View
 } from 'react-native';
+// React Navigation
+import {
+  StackActions,
+  useNavigation
+} from '@react-navigation/native';
+// Components
+import { PrimaryButton } from '../../components';
+// Theme
+import { globalStyles } from '../../theme';
 
 
 export const SettingsScreen = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text style={{ color: 'white' }}>SettingsScreen</Text>
+    <View style={ globalStyles.container }>
+      <Text style={{ marginBottom: 10 }}>Settings Screen</Text>
+
+      <PrimaryButton
+        label='Regresar'
+        onPress={ () => navigation.goBack() }
+      />
+
+      <PrimaryButton
+        label='Regresar al Home'
+        onPress={ () => navigation.dispatch( StackActions.popToTop() ) }
+      />
     </View>
   );
 }
